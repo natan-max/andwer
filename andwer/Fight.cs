fbd8c1d1a8d422b8843ab8366ed28a89b2f5e3ba
Nazx1xk <g.nazar2009@gmail.com>
2024-10-01 19:56:04 +0300

    Update Fight.cs

--- andwer/Fight.cs
+++ andwer/Fight.cs
@@ -8,52 +8,115 @@ namespace andwer
 {
     internal class Fight
     {
-        class Mob 
+        class Mob
         {
-            public double Health { get; private set; }
-            public double MaxHealth { get; private set; }
-            public double Damage { get; private set; }
-            public string Name { get; private set; }
+            public double Health { get; protected set; }
+            public double MaxHealth { get; protected set; }
+            public double Damage { get; protected set; }
+            public string Name { get; protected set; }
 
             public bool IsAlive => Health > 0;
 
-            public void ReceiveDamage(double damage) 
-            { 
-                for(double i = Health; i > 0; i--)
+            public virtual void ReceiveDamage(double damage)
+            {
+                Health -= damage;
+                if (Health < 0)
                 {
-                    if (Health <= 0)
-                    {
-                      
-                    }
+                    Health = 0;
                 }
+                Console.WriteLine($"{Name} отримав {damage} шкоди. Залишилось здоров'я: {Health}");
             }
-            public void DealDamage(Mob mob) { ... }
+
+            public virtual void DealDamage(Mob mob)
+            {
+                mob.ReceiveDamage(Damage);
+            }
         }
 
-        
         class Player : Mob
         {
+            private int shieldTurns = 0;
+
+            public Player(string name, double health, double damage)
+            {
+                Name = name;
+                MaxHealth = health;
+                Health = MaxHealth;
+                Damage = damage;
+            }
+
             public void Shield()
-            { 
-            
+            {
+                if (shieldTurns == 0)
+                {
+                    shieldTurns = 2;
+                    Console.WriteLine($"{Name} активував щит на 2 ходи.");
+                }
+                else
+                {
+                    Console.WriteLine("Щит вже активовано.");
+                }
+            }
+
+            public override void ReceiveDamage(double damage)
+            {
+                if (shieldTurns > 0)
+                {
+                    Console.WriteLine($"{Name} заблокував атаку щитом.");
+                    shieldTurns--;
+                }
+                else
+                {
+                    base.ReceiveDamage(damage);
+                }
             }
         }
 
-        
         class Zombie : Mob
         {
-
-        }
-
-        
-        class Enderman : Mob
-        {
-            public void Stealth()
+            public Zombie(string name, double health, double damage)
             {
-
+                Name = name;
+                MaxHealth = health;
+                Health = MaxHealth;
+                Damage = damage;
             }
         }
 
+        class Enderman : Mob
+        {
+            public Enderman(string name, double health, double damage)
+            {
+                Name = name;
+                MaxHealth = health;
+                Health = MaxHealth;
+                Damage = damage;
+            }
+
+            public bool Stealth()
+            {
+                Random random = new Random();
+                int stealth = random.Next(0, 11);
+                if (stealth < 6)
+                {
+                    Console.WriteLine($"{Name} ухилився від атаки!");
+                    return true;
+                }
+                return false;
+            }
+
+            public override void ReceiveDamage(double damage)
+            {
+                if (!Stealth())
+                {
+                    base.ReceiveDamage(damage);
+                }
+                else
+                {
+                    Console.WriteLine($"{Name} уникнув шкоди!");
+                }
+            }
+        }
 
     }
 }
