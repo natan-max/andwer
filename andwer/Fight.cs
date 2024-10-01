67e908e3f18217c346426c4006b5fc64227d15ed
Nazx1xk <g.nazar2009@gmail.com>
2024-10-01 19:25:56 +0300

    Update Fight.cs

--- andwer/Fight.cs
+++ andwer/Fight.cs
@@ -8,5 +8,37 @@ namespace andwer
 {
     internal class Fight
     {
+        class Mob 
+        {
+            public double Health { get; private set; }
+            public double MaxHealth { get; private set; }
+            public double Damage { get; private set; }
+            public string Name { get; private set; }
+
+            public bool IsAlive => Health > 0;
+
+            public void ReceiveDamage(double damage) { ... }
+            public void DealDamage(Mob mob) { ... }
+        }
+
+        
+        class Player : Mob
+        {
+            public void 
+        }
+
+        
+        class Zombie : Mob
+        {
+            public void Burn() { ... }
+        }
+
+        
+        class Creeper : Mob
+        {
+            public void Explode() { ... }
+        }
+
+
     }
 }
